fd7d4fbfb3f6959e092918076505fedae8d54c69
savus <justin.fuzzygreenstuff@gmail.com>
2026-07-13 13:38:37 -0600

    adjust animation properties of css slideFadeLeft and slideFadeRight

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,7 @@
 import { API_REQUESTS } from "./api.js";
 import Card from "./Card.js";
 import { CSS_CLASSES, pokeNames } from "./constants.js";
-<<<<<<< HEAD
-import { buildCardHTML, generateCardData, createAndAppendAllCards, wait, } from "./utility.js";
-=======
 import { buildCardHTML, generateCardData, createAndAppendAllCards, animateElement, wait, displayGameMessage, } from "./utility.js";
->>>>>>> refs/remotes/origin/feature/CardClickEvent
 export const dummyColors = [
     "#e80a0a",
     "#0ed3e5",
@@ -43,91 +39,7 @@ document.addEventListener("keyup", async (e) => {
         case "Enter":
             const messageContainer = document.querySelector(".message-container");
             const gameMessage = document.querySelector(".game-message");
-<<<<<<< HEAD
-            const displayMessage = document.querySelector(".display-message");
-            function transitionElement(element, className, addOrRemove) {
-                return new Promise((resolve) => {
-                    const handleTransitionEnd = () => {
-                        element.removeEventListener("transitionend", handleTransitionEnd);
-                        resolve(element);
-                    };
-                    element.addEventListener("transitionend", handleTransitionEnd);
-                    addOrRemove
-                        ? element.classList.add(className)
-                        : element.classList.remove(className);
-                });
-            }
-            await transitionElement(gameMessage, "visible", true);
-            await transitionElement(displayMessage, "showtime", true);
-            await wait(300);
-            await transitionElement(displayMessage, "showtime", false);
-            await transitionElement(gameMessage, "visible", false);
-            await wait(300);
-            await transitionElement(gameMessage, "visible", true);
-            await transitionElement(displayMessage, "showtime", true);
-            // const anim1 = gameMessage?.animate(
-            //   {
-            //     opacity: [0, 1],
-            //     transform: ["translateX(-100px)", "translateX(0)"],
-            //   },
-            //   {
-            //     fill: "forwards",
-            //     duration: 500,
-            //   },
-            // );
-            // await anim1?.finished;
-            // gameMessage?.classList.add("showtime");
-            // await wait(1000);
-            // gameMessage?.classList.remove("showtime");
-            // await wait(500);
-            // const anim2 = gameMessage?.animate(
-            //   {
-            //     opacity: [1, 0],
-            //     transform: ["translateX(0)", "translateX(-100px)"],
-            //   },
-            //   {
-            //     fill: "forwards",
-            //     duration: 500,
-            //   },
-            // );
-            // await anim2?.finished;
-            // const anim1 = displayMessage?.animate(
-            //   {
-            //     opacity: [0, 1],
-            //     transform: ["scale(0)", "scale(1)"],
-            //   },
-            //   {
-            //     fill: "forwards",
-            //     duration: 1000,
-            //   },
-            // );
-            // await anim1?.finished;
-            // const anim2 = displayMessage?.animate(
-            //   {
-            //     opacity: [1, 0],
-            //     transform: ["translateX(0)", "translateX(100px)"],
-            //   },
-            //   {
-            //     fill: "forwards",
-            //     duration: 1000,
-            //   },
-            // );
-            // await anim2?.finished;
-            // alert("Animations finished");
-            // const anim1 = displayMessage?.animate([{ opacity: 0 }, { opacity: 1 }], {
-            //   duration: 1000,
-            //   fill: "forwards",
-            // });
-            // await anim1?.finished;
-            // const anim2 = displayMessage?.animate(
-            //   [{ transform: "translateX(0px)" }, { transform: "translateX(100px)" }],
-            //   { duration: 500, fill: "forwards" },
-            // );
-            // await anim2?.finished;
-            console.log("animations finished");
-=======
             await displayGameMessage(messageContainer);
->>>>>>> refs/remotes/origin/feature/CardClickEvent
     }
 });
 //# sourceMappingURL=app.js.map
