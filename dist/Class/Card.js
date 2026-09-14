4d79c5b35c26a8ab0392419b8e38ff197a1f611b
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2026-09-14 04:48:34 -0600

    branch init

---
+++ dist/Class/Card.js
@@ -0,0 +1,65 @@
+import { cardOnClickHandler } from "../clickEvents.js";
+import { DATA_FACE_POSIION, DOWN, UP } from "../constants.js";
+import { animateElement } from "../utility.js";
+class Card {
+    html;
+    cardData;
+    state;
+    constructor(htmlData, cardData) {
+        this.html = htmlData;
+        this.cardData = cardData;
+        this.state = {
+            isClickable: true,
+            facePosition: "down",
+            isFlippable: true,
+        };
+        this.html.addEventListener("click", () => {
+            if (this.state.isClickable)
+                return cardOnClickHandler(this);
+            console.log("card is not clickable");
+        });
+    }
+    flipCard = (direction = "toggle", ignoreState = false) => {
+        if (!this.state.isFlippable && !ignoreState)
+            return;
+        const up = () => {
+            this.state.facePosition = UP;
+            this.html.dataset.facePosition = UP;
+        };
+        const down = () => {
+            this.state.facePosition = DOWN;
+            this.html.dataset.facePosition = DOWN;
+        };
+        const toggle = () => {
+            if (this.state.facePosition === "up") {
+                up();
+            }
+            else {
+                down();
+            }
+        };
+        switch (direction) {
+            case "up":
+                up();
+                break;
+            case "down":
+                down();
+                break;
+            case "toggle":
+                toggle();
+                break;
+        }
+    };
+    selectCard = () => {
+        this.flipCard("up");
+        this.state.isClickable = false;
+        this.state.isFlippable = false;
+    };
+    deSelectCard = async () => {
+        await animateElement(this.html, { type: DATA_FACE_POSIION, value: "down" }, "transitionend");
+        this.state.isClickable = true;
+        this.state.isFlippable = true;
+    };
+}
+export default Card;
+//# sourceMappingURL=Card.js.map
---
+++ src/Class/CardFactory.ts
@@ -0,0 +1,63 @@
+import { allCards, cardContainer } from "../app.js";
+import type { TPokemonData, TPokemonDom } from "../types.js";
+import { shuffleInPlace } from "../utility.js";
+import Card from "./Card.js";
+
+const buildCardHTML = (data: TPokemonData) => {
+  const card: TPokemonDom = document.createElement("div");
+  card.dataset.facePosition = "down";
+  card.className = `card-outer ${data.isDummyData ? "dummy-card" : ""}`;
+  const cardHTMLString = `<div class="card-body">
+      <div class="card-inner">
+        <div class="card-name">${data.name}</div>
+        <div class="img-container">
+          <div class="card-img">
+            <img
+              class="img"
+              src="${data.img}"
+            />
+            <div class="dummy-color" style={${data.isDummyData ? data.img : ""}></div>
+          </div>
+        </div>
+        <div>Type: ${data.type}</div>
+        <div class="stats">
+          <div>hp: ${data.hp}</div>
+          <div>attack: ${data.attack}</div>
+          <div>defense: ${data.defense}</div>
+          <div>special_attack: ${data.special_attack}</div>
+          <div>special_defense: ${data.special_defense}</div>
+          <div>speed: ${data.speed}</div>
+        </div>
+      </div>
+      <div class="card-back"></div>
+    </div>`;
+  card.insertAdjacentHTML("beforeend", cardHTMLString);
+  card.metaData = data;
+  return card;
+};
+
+export const createAndAppendCard = (data: TPokemonData) => {
+  const cardHTML = buildCardHTML(data);
+  const card = new Card(cardHTML, data);
+  allCards.push(card);
+  return cardContainer.appendChild(cardHTML);
+};
+
+const createAndAppendAllCards = (array: TPokemonData[]) => {
+  let shuffledArray: TPokemonData[] = [];
+
+  for (let i = 0; i <= 10; i++) {
+    shuffledArray = shuffleInPlace(array);
+  }
+
+  shuffledArray.forEach((mon) => {
+    createAndAppendCard(mon);
+  });
+};
+
+const CardFactory = {
+  createAndAppendAllCards,
+  createAndAppendCard,
+};
+
+export default CardFactory;
